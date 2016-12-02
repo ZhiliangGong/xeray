6e54b310fdf4be844e6e986a1afc2772afc0b9d0
Zhiliang Gong <zhiliang.gong@gmail.com>
2016-12-02 12:54:42 -0600

    old edition working well

--- functions/test2.m
+++ functions/test2.m
@@ -1,8 +1,6 @@
 clear;
-s = xlayers(3);
-s.push(1, 0.25, 12);
-s.push(2, 0.4, 8);
-s.push(3, 1, Inf, 'H2OCa0.009Cl0.018');
+s = xlayers(1);
+s.push(1, 1, Inf, 'H2OCa0.009Cl0.018');
 s.refractionIndex(10);
 s.optics(linspace(0.001, 0.004, 10));
 s.calculateFluoIntensity('Ca');
--- functions/xlayers.m
+++ functions/xlayers.m
@@ -4,9 +4,10 @@ classdef xlayers < handle
     
     properties
         % basic
-        N
+        N % number of layers
+        M % number of incidence angles
         density
-        electronDensity
+        eDensity
         thickness
         formula
         
@@ -27,7 +28,7 @@ classdef xlayers < handle
         transmission % complex amplitude
         reflection % complex amplitude
         penetration % depth in A
-        intensity % relative to incoming beam intensity
+        intensity % relative to incoming beam intensity at the start of each layer
         
         % fitting
         fitElement
@@ -43,7 +44,7 @@ classdef xlayers < handle
             s.N = n;
             s.formula = cell(1, n);
             s.density = zeros(1, n);
-            s.electronDensity = zeros(1, n);
+            s.eDensity = zeros(1, n);
             s.thickness = zeros(1, n);
             s.elements = cell(1, n);
             s.stoichiometry = cell(1, n);
@@ -57,7 +58,7 @@ classdef xlayers < handle
             switch length(varargin)
                 case 3 % unkown formula, give the electron density and thickness
                     n = varargin{1};
-                    s.electronDensity(n) = varargin{2};
+                    s.eDensity(n) = varargin{2};
                     s.thickness(n) = varargin{3};
                 case 4 % known formula, give the density, thickness, and formula
                     n = varargin{1};
@@ -72,14 +73,14 @@ classdef xlayers < handle
         % calculate x-ray optical properties
         function refractionIndex(s, energy)
             
-            if find(~(s.density | s.electronDensity), 1)
+            if find(~(s.density | s.eDensity), 1)
                 error('Either electron density or the density must be present for each layer.');
             end
             s.energy = energy;
             s.wavelength = getWavelength(energy);
             for i = 1 : s.N
                 if s.density(i)
-                    s.parseFormula(i);
+                    [s.elements{i}, s.stoichiometry{i}] = parseFormula(s.formula{i});
                     s.calculateDispersionAbsorption(i);
                 else
                     s.calculateDispersion(i);
@@ -91,9 +92,9 @@ classdef xlayers < handle
         % calculate the detailed optics
         function optics(s, angle)
             
-            M = length(angle);
+            s.M = length(angle);
             s.angle = reshape(angle, numel(angle), 1);
-            s.rangle = sqrt(repmat(s.angle, 1, s.N).^2 - 2 * repmat(s.dispersion, M, 1) + 2i * repmat(s.absorption, M, 1));
+            s.rangle = sqrt(repmat(s.angle, 1, s.N).^2 - 2 * repmat(s.dispersion, s.M, 1) + 2i * repmat(s.absorption, s.M, 1));
             
             % calculate the refraction matrices
             if s.thickness(end) ~= Inf
@@ -102,23 +103,23 @@ classdef xlayers < handle
             
             theta1 = [s.angle, s.rangle(:, 1:end-1)];
             theta2 = s.rangle;
-            d1 = repmat([0, s.thickness(:, 1:end-1)], M, 1);
-            d2 = repmat([s.thickness(1:end-1), 0], M, 1); % calculate the intensity at the interface for the last layer
+            d1 = repmat([0, s.thickness(:, 1:end-1)], s.M, 1);
+            d2 = repmat([s.thickness(1:end-1), 0], s.M, 1); % calculate the intensity at the interface for the last layer
             
             ratio1 = (theta1 + theta2) / 2 ./ theta1;
             ratio2 = (theta1 - theta2) / 2 ./ theta1;
             expo1 = 1i * pi / s.wavelength * (theta1 .* d1 + theta2 .* d2);
             expo2 = 1i * pi / s.wavelength * (theta1 .* d1 - theta2 .* d2);
             
-            m = zeros(M, s.N, 4);
+            m = zeros(s.M, s.N, 4);
             m(:, :, 1) = ratio1 .* exp(-expo1);
             m(:, :, 2) = ratio2 .* exp(-expo2);
             m(:, :, 3) = ratio2 .* exp(expo2);
             m(:, :, 4) = ratio1 .* exp(expo1);
             
-            tempmatrix = cell(M, s.N);
-            s.matrices = cell(M, s.N);
-            for i = 1 : M
+            tempmatrix = cell(s.M, s.N);
+            s.matrices = cell(s.M, s.N);
+            for i = 1 : s.M
                 for j = s.N : -1 : 1
                     tempmatrix{i, j} = reshape(m(i, j, :), 2, 2)';
                     s.matrices{i, j} = eye(2);
@@ -128,15 +129,15 @@ classdef xlayers < handle
                 end
             end
             
-            tamp = zeros(M, s.N);
-            ramp = zeros(M, s.N);
+            tamp = zeros(s.M, s.N);
+            ramp = zeros(s.M, s.N);
             
             ramp(:, end) = 0;
-            for i = 1 : M
+            for i = 1 : s.M
                 tamp(i, end) = 1 / s.matrices{i, 1}(1, 1);
             end
             
-            for i = 1 : M
+            for i = 1 : s.M
                 for j = 1 : s.N - 1
                     tamp(i, j) = s.matrices{i, j+1}(1, 1) * tamp(i, end);
                     ramp(i, j) = s.matrices{i, j+1}(2, 1) * tamp(i, end);
@@ -145,17 +146,18 @@ classdef xlayers < handle
             
             
             phaseLength = cumsum([0, s.thickness(1:end-1)]) - [s.thickness(1:end-1), 0] / 2;
-            phaseShift = 2 * pi / s.wavelength * s.rangle .* repmat(phaseLength, M, 1);
+            phaseShift = 2 * pi / s.wavelength * s.rangle .* repmat(phaseLength, s.M, 1);
             s.transmission = tamp .* exp( 1i * phaseShift );
             s.reflection = ramp .* exp( -1i * phaseShift );
             
             alpha = repmat(s.angle, 1, s.N);
-            delta = repmat(s.dispersion, M, 1);
-            beta = repmat(s.absorption, M, 1);
+            delta = repmat(s.dispersion, s.M, 1);
+            beta = repmat(s.absorption, s.M, 1);
             s.penetration = s.wavelength / 4 / pi ./ imag( sqrt( alpha.^2 - 2 * delta + 2i * beta ) );
             
             % intensity below each of the interface
-            s.intensity = abs((s.transmission + s.reflection).^2);
+            attenuation = exp( - repmat(cumsum([0, s.thickness(1: end-1)]), s.M, 1) ./ [ones(s.M, 1), s.penetration(:, 1:end-1)]);
+            s.intensity = abs((s.transmission + s.reflection).^2) .* attenuation;
             
         end
         
@@ -164,74 +166,26 @@ classdef xlayers < handle
             
             s.fitElement = element;
             
-            location = false(1, s.N); % locate which layer the element is in
+            location = zeros(1, s.N); % locate which layer the element is in
             for i = 1 : length(s.elements)
                 if ~isempty(s.elements{i})
                     for j = 1 : length(s.elements{i})
                         if strcmp(s.elements{i}{j}, element)
-                            location(i) = true;
+                            location(i) = 1;
                             break;
                         end
                     end
                 end
             end
             
-            cumsum([s.thickness(1: end-1), 0])
-            layerIntensity = repmat(location, length(s.angle), 1) .* s.intensity;
+            z = repmat(cumsum([0, s.thickness]), s.M, 1);
+            zdiff = z(:, 2:end) - z(:, 1:end-1);
+            integralFactor = s.penetration .* (exp(-z(:, 1:end-1) ./ s.penetration) - exp(-z(:, 2:end) ./ s.penetration));
+            infIndex = (s.penetration == Inf);
+            integralFactor(infIndex) = zdiff(infIndex);
+            layerIntensity = repmat(location, s.M, 1) .* s.intensity .* integralFactor;
             s.fluoIntensity = sum(layerIntensity, 2);
             
-        end
-        
-        % parse formula to obtain elements and stoichiometry
-        function parseFormula(s, k)
-            
-            f = s.formula{k};
-
-            % check errors in formula
-            if ~isempty(regexp(f, '[^[A-Z, a-z, \., 0-9]', 'once'))
-                error('Formula contains illegal characters!');
-            elseif isempty(regexp(f, '^[A-Z]', 'once'))
-                error('Formula should start with a capital letter!');
-            elseif ~isempty(regexp(f, '\.[0-9]\.', 'once')) || ~isempty(regexp(f, '\.[A-Z, a-z\', 'once'))
-                error('Check decimal point position!');
-            end
-
-            % insert 1's when missing for stoichiometry
-            upperCase = (f <= 'Z' & f >= 'A');
-            lowerCase = (f <= 'z' & f >= 'a');
-            marker = ((upperCase | lowerCase) & [upperCase(2:end), true]);
-            location = false(1, length(marker) + sum(marker));
-            location(find(marker)+(1:length(find(marker)))) = true;
-            f(~location) = f;
-            f(location) = '1';
-
-            % assign logical arrays
-            if length(upperCase) < length(f)
-                upperCase = (f <= 'Z' & f >= 'A');
-                lowerCase = (f <= 'z' & f >= 'a');
-            end
-            number = ((f <= '9' & f >= '0') | f == '.');
-            n = sum(upperCase);
-            elem = cell(1,n);
-            stoi = zeros(1,n);
-
-            % obtain elements
-            start = find(upperCase);
-            finish = find((upperCase & ~[lowerCase(2:end),false]) | (lowerCase & ~[lowerCase(2:end),false]));
-            for i = 1:n
-                elem{i} = f(start(i):finish(i));
-            end
-
-            % obtain stoichiometry numbers
-            start = find(number & ~[true, number(1:end-1)]);
-            finish = find(number & ~[number(2:end), false]);
-            for i = 1:n
-                stoi(i) = str2double(f(start(i):finish(i)));
-            end
-            
-            s.elements{k} = elem;
-            s.stoichiometry{k} = stoi;
-
         end
         
         % calculate dispersion and absorption
@@ -268,13 +222,13 @@ classdef xlayers < handle
             % calculate the dispersion part of the refractive index, the real part
 
             % units
-            % electronDensity - /A^3, for water is 0.3344
+            % eDensity - /A^3, for water is 0.3344
             % energy - keV
             % wavelength - A
 
             re = 2.81794092e-5; % classical radius for electron in A
 
-            s.dispersion(k) = re * s.electronDensity(k) * s.wavelength^2 / 2 / pi;
+            s.dispersion(k) = re * s.eDensity(k) * s.wavelength^2 / 2 / pi;
 
         end
         
