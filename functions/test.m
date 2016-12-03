45b93978787f5197746b029e21c24daf02b8520d
Zhiliang Gong <zhiliang.gong@gmail.com>
2016-12-02 21:16:11 -0600

    xlayers progress

--- functions/test.m
+++ functions/test.m
@@ -1,8 +1,11 @@
 clear;
-s = xlayers(3);
-s.push(1, 0.25, 12);
-s.push(2, 0.4, 8);
-s.push(3, 1, Inf, 'H2OCa0.009Cl0.018');
-s.refractionIndex(10);
-s.optics(linspace(0.001, 0.004, 10));
+clc;
+s = xlayers;
+s.updateLayer(1, 0.25, 12);
+s.updateLayer(2, 0.4, 8);
+s.updateLayer(3, 0.334, Inf, 'H2OCa0.009Cl0.018');
+calculateRefractionIndex(s, 10);
+angle = linspace(0.001, 0.004, 10);
+s.optics(angle);
 s.calculateFluoIntensity('Ca');
+s
