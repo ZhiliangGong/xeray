fb5138c91818f5626caf833f921304f4c886d875
Zhiliang Gong <zhiliang.gong@gmail.com>
2017-03-29 11:23:03 -0500

    fixed typo

--- functions/ChemicalFormula.m
+++ functions/ChemicalFormula.m
@@ -17,7 +17,7 @@ classdef ChemicalFormula < handle
         
         function this = ChemicalFormula()
             
-            this.generatePeriodicTalbe();
+            this.generatePeriodicTable();
             
         end
         
@@ -80,7 +80,7 @@ classdef ChemicalFormula < handle
             
         end
         
-        function generatePeriodicTalbe(this)
+        function generatePeriodicTable(this)
 
             symbols = 'H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U';
             atomicNumber = (1:92);
